140981955613a05a4af6c38ca4215b6c431149b2
Jonny Buchanan <jonathan.buchanan@gmail.com>
2019-01-31 12:06:02 +1000

    Move duration picker into its own package

---
+++ lib/durationpicker.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:numberpicker/numberpicker.dart';
+
+class DurationPickerDialog extends StatefulWidget {
+  final Duration initialDuration;
+  final EdgeInsets titlePadding;
+  final Widget title;
+  final Widget confirmWidget;
+  final Widget cancelWidget;
+
+  DurationPickerDialog(
+      {@required this.initialDuration,
+      this.title,
+      this.titlePadding,
+      Widget confirmWidget,
+      Widget cancelWidget})
+      : confirmWidget = confirmWidget ?? new Text('OK'),
+        cancelWidget = cancelWidget ?? new Text('CANCEL');
+
+  @override
+  State<StatefulWidget> createState() =>
+      new _DurationPickerDialogState(initialDuration);
+}
+
+class _DurationPickerDialogState extends State<DurationPickerDialog> {
+  int minutes;
+  int seconds;
+
+  _DurationPickerDialogState(Duration initialDuration) {
+    minutes = initialDuration.inMinutes;
+    seconds = initialDuration.inSeconds % Duration.secondsPerMinute;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new AlertDialog(
+      title: widget.title,
+      titlePadding: widget.titlePadding,
+      content: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        new NumberPicker.integer(
+            listViewWidth: 65,
+            initialValue: minutes,
+            minValue: 0,
+            maxValue: 10,
+            onChanged: (value) {
+              this.setState(() {
+                minutes = value;
+              });
+            }),
+        Text(
+          ':',
+          style: TextStyle(fontSize: 30),
+        ),
+        new NumberPicker.integer(
+            listViewWidth: 65,
+            initialValue: seconds,
+            minValue: 0,
+            maxValue: 59,
+            onChanged: (value) {
+              this.setState(() {
+                seconds = value;
+              });
+            }),
+      ]),
+      actions: [
+        new FlatButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: widget.cancelWidget,
+        ),
+        new FlatButton(
+            onPressed: () => Navigator.of(context)
+                .pop(new Duration(minutes: minutes, seconds: seconds)),
+            child: widget.confirmWidget),
+      ],
+    );
+  }
+}
