3849a519074671c3f5f83b1b98f82784bc7677e1
Jonny Buchanan <jonathan.buchanan@gmail.com>
2019-03-06 21:54:41 +1000

    Simplify utils.formatTime

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,7 +1,5 @@
 String formatTime(Duration duration) {
-  int seconds = (duration.inMilliseconds / 1000).truncate();
-  int minutes = (seconds / 60).truncate();
-  String displayMinutes = (minutes % 60).toString().padLeft(2, '0');
-  String displaySeconds = (seconds % 60).toString().padLeft(2, '0');
-  return '$displayMinutes:$displaySeconds';
+  String minutes = (duration.inMinutes).toString().padLeft(2, '0');
+  String seconds = (duration.inSeconds % 60).toString().padLeft(2, '0');
+  return '$minutes:$seconds';
 }
